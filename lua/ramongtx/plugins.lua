9939178fceca9fdfa7b84c1d6a2f78197da00364
Ramon Carvalho Maciel <ramoncm@amazon.com>
2023-05-26 16:51:54 +0100

    Add neovim-tree

--- lua/ramongtx/plugins.lua
+++ lua/ramongtx/plugins.lua
@@ -53,6 +53,16 @@ local plugins = {
       }
   },
   {
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      dependencies = {
+          "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+          require("nvim-tree").setup {}
+      end,
+  },
+  {
       'numToStr/Comment.nvim',
       config = function()
           require('Comment').setup()
