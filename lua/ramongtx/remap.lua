876d80d89af666ca068c0eded4ef027f0fe879df
Ramon Carvalho Maciel <ramongtx@gmail.com>
2023-06-27 07:28:30 +0100

    Finish remapping to wk

--- lua/ramongtx/remap.lua
+++ lua/ramongtx/remap.lua
@@ -11,37 +11,40 @@ wk.register({
     },
     ["<leader>pv"] = { ":NvimTreeFocus<CR>", "Open explorer" },
     ["<leader>r"] = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Replace word under cursor" },
+    ["J"] = { "mzJ`z", "Append next line to this" },
+
+    -- Scroll and recenter
+    ["<C-d>"] = { "<C-d>zz", "which_key_ignore" },
+    ["<C-u>"] = { "<C-u>zz", "which_key_ignore" },
+
+    -- Keep cursor in the middle after seach
+    ["n"] = { "nzzzv", "which_key_ignore" },
+    ["N"] = { "Nzzzv", "which_key_ignore" },
+
+    ['<leader>w'] = { [[:%s/\s\+$//e<cr>]], "Trim whitespace" },
+    ["<leader>Y"] = { [["+Y]], "Copy line to OS clipboard" },
+    ["<leader>y"] = { [["+y]], "Copy selection to OS clipboard" },
+    ["<leader>d"] = { [["_d]], "Delete to void" },
+})
+
+wk.register({
+    J = { ":m '>+1<CR>gv=gv", "Move selection down" },
+    K = { ":m '<-2<CR>gv=gv", "Move selection up" },
+    ["<leader>y"] = { [["+y]], "Copy selection to OS clipboard" },
+    ["<leader>d"] = { [["_d]], "Delete to void" },
+}, { mode = "v" })
+
+wk.register({
+    -- In Insertion mode, C-c is escape
+    ["<C-c>"] = { "<Esc>", "which_key_ignore" },
+}, { mode = "i", })
+
+wk.register({
+    -- Quick exit
+    ["<C-q>"] = { ":qa<CR>", "which_key_ignore" },
+    ["<C-S-q>"] = { ":qa!<CR>", "which_key_ignore" },
+}, {
+    mode = { "i", "n", "v" }
 })
 
 
--- Move selection with context
-kmap("v", "J", ":m '>+1<CR>gv=gv")
-kmap("v", "K", ":m '<-2<CR>gv=gv")
-
--- Append next line to this
-kmap("n", "J", "mzJ`z")
-
--- Scroll and recenter
-kmap("n", "<C-d>", "<C-d>zz")
-kmap("n", "<C-u>", "<C-u>zz")
-
--- Keep cursor in the middle after seach
-kmap("n", "n", "nzzzv")
-kmap("n", "N", "Nzzzv")
-
--- Copy to OS clipboard 
-kmap({"n", "v"}, "<leader>y", [["+y]])
-kmap("n", "<leader>Y", [["+Y]])
-
--- Delete to void
-kmap({"n", "v"}, "<leader>d", [["_d]])
-
--- In Insertion mode, C-c is escape
-kmap("i", "<C-c>", "<Esc>")
-
--- Quick exit
-kmap({"i", "n", "v"}, "<C-q>", ":qa<CR>")
-kmap({"i", "n", "v"}, "<C-S-q>", ":qa!<CR>")
-
--- trim whitespace
-kmap('n', '<leader>wt', [[:%s/\s\+$//e<cr>]])
